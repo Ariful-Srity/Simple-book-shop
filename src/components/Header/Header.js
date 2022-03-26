4819ce58f444054c6ab8f6f259f67eb9fb77d414
Ariful-Srity <arifulontor84@gmail.com>
2022-03-26 23:00:52 +0600

    add file

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,8 @@ import './Header.css'
 const Header = () => {
     return (
         <div className='header-info'>
-            <h1>Modina Book Store</h1>
-            <h3>Choose your favourite book!!</h3>
+            <h1>Simple Book Shop</h1>
+            <h3>Buy your best book here</h3>
         </div>
     );
 };
